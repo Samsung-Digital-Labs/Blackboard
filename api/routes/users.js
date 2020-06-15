13169c0b2a44e9f572a806f0520b443f82c109d7
Aadhar <tyagi.aadhar@gmail.com>
2020-06-15 15:58:22 +0530

    add already exisiting user check

--- api/routes/users.js
+++ api/routes/users.js
@@ -5,29 +5,47 @@ const bcrypt = require('bcrypt');
 
 const User = require ('./../models/user');
 
+
 router.post('/signup', (req, res, nxt) => {
-    bcrypt.hash(req.body.password, 10, (err, hash) => {
-        if (err) {
-            res.status(500).json ({error: err});
-        } else {
-            const user = new User({
-                _id : new mongoose.Types.ObjectId(),
-                email : req.body.email,
-                password : hash  
+    // Find the email in database
+    // If exists throw error else
+    // encrypt password and create user
+    User.find({email: req.body.email})
+    .exec()
+    .then(user => {
+        if (user) {
+            return res.status(409).json({error: "User Already Exists"})
+        }
+        else {
+            bcrypt.hash(req.body.password, 10, (err, hash) => {
+                if (err) {
+                    res.status(500).json ({error: err});
+                } 
+                else {
+                    const user = new User({
+                        _id : new mongoose.Types.ObjectId(),
+                        email : req.body.email,
+                        password : hash  
+                    });
+                    user.save()
+                    .then(result => {
+                        console.log(result)
+                        res.status(200).json({
+                            message: 'User Created'
+                        });
+                    })
+                    .catch(err => {
+                        console.group(err);
+                        res.status(500).json({
+                            error: err
+                        });
+                    });
+                }   
             });
-            user.save()
-            .then(result => {
-                res.status(200).json({
-                    message: 'User Created'
-                });
-            })
-            .catch(err => {
-                console.group(err);
-                res.status(500).json({
-                    error: err
-                });
-            });
-        }   
+        }
+    })
+    .catch(err => {
+        res.status(500).json ({error: 'Problem connecting to server'})
     });
 });
 
