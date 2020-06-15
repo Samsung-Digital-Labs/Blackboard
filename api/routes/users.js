b31d0d179cd0779b9ebd6ec756a17822d086eea3
Aadhar <tyagi.aadhar@gmail.com>
2020-06-15 16:58:49 +0530

    add login route

--- api/routes/users.js
+++ api/routes/users.js
@@ -49,6 +49,39 @@ router.post('/signup', (req, res, nxt) => {
     });
 });
 
+
+router.post ('/login', (req, res, nxt) => {
+    User.find({email: req.body.email})
+    .exec()
+    .then(user => {
+        if (user.length < 1) {
+            return res.status(401).json ({
+                message: 'Auth failed'
+            });
+        }
+        bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+            if (err) {
+                return res.status(401).json ({
+                    message: 'Auth failed'
+                });
+            }
+            if (result) {
+                return res.status(200).json ({
+                    message : "User logged in"
+                });
+            } else {
+                return res.status(401).json ({
+                    message: 'Auth failed'
+                });
+            }
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json ({error: "Cannot contact database"});
+    });
+});
+
 router.delete('/:userID', (req, res, nxt) => {
     User.remove({_id : req.param.userID})
     .exec()
