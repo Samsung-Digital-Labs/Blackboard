71f4c21e28350f886008f3328c51831271db5421
Aadhar <tyagi.aadhar@gmail.com>
2020-06-21 19:39:06 +0530

    add reducer cases

--- client/src/data/users/reducers/loadUserReducer.tsx
+++ client/src/data/users/reducers/loadUserReducer.tsx
@@ -1,13 +1,29 @@
 import * as actionTypes from "../actions/actionTypes";
 
-const loadUserReducer = (state = {}, action: any) => {
+const loadUserReducer = (
+  state = {
+    userName: "",
+    darkMode: false,
+    isUserLoggedIn: false,
+  },
+  action: any
+) => {
   switch (action.type) {
     case actionTypes.LOAD_USER:
       return {
         ...state,
         isUserLoggedIn: action.payload.isUserLoggedIn,
-
       };
+    case actionTypes.DARK_MODE:
+      return {
+        ...state,
+        darkMode: action.payload.darkMode
+      };
+    case actionTypes.SET_NAME:
+      return {
+        ...state, 
+        userName: action.payload.name
+      }
     default:
       return state;
   }
