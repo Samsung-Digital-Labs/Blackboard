ce89d470731b56b1543a2e77b3ca4ef8a9635762
Aadhar <tyagi.aadhar@gmail.com>
2020-07-01 10:44:51 +0530

    complete create classroom

--- client/src/pages/CreateClassroom/CreateClassroom.tsx
+++ client/src/pages/CreateClassroom/CreateClassroom.tsx
@@ -1,106 +1,147 @@
-import React, { Component } from 'react';
-import { IonItem, IonLabel, IonInput, IonTextarea, IonGrid, IonCol, IonRow, IonButton } from '@ionic/react';
-import { Link } from 'react-router-dom';
-import axios from 'axios';
+import React, { Component } from "react";
+import {
+  IonItem,
+  IonLabel,
+  IonInput,
+  IonTextarea,
+  IonGrid,
+  IonCol,
+  IonRow,
+  IonButton,
+  IonContent,
+} from "@ionic/react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
-import {connect} from 'react-redux';
+import { connect } from "react-redux";
 
-interface Props{
-    history:any,
-    email:string
+interface Props {
+  history: any;
+  userID: string;
+}
+
+interface State {
+  classroomName: string;
+  subject: string;
+  description: string;
+  phone: string;
+}
+
+class CreateClassroom extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      classroomName: "",
+      subject: "",
+      description: "",
+      phone: "",
+    };
+  }
+
+  create = () => {
+    const classroom = {
+      classroomName: this.state.classroomName,
+      subject: this.state.subject,
+      description: this.state.description,
+      userID: this.props.userID,
+      phone: this.state.phone,
+    };
+
+    // api request
+    axios
+      .post("/classrooms/create", classroom)
+      .then((response) => {
+        this.props.history.push("/page/classrooms");
+      })
+      .catch((err) => {
+        console.log(err);
+        window.alert("error in creating classroom");
+      });
+  };
+
+  render() {
+
+    return (
+      <IonContent className="ion-text-center">
+        <h1>Create Classroom</h1>
+        <br></br>
+        <IonGrid>
+          <IonRow className="ion-justify-content-center">
+            <IonCol sizeXs="12" sizeMd="6">
+              <IonItem>
+                <IonLabel position="floating">Classroom Name</IonLabel>
+                <IonInput
+                  onIonChange={(e) => {
+                    this.setState({ classroomName: e.detail.value! });
+                  }}
+                ></IonInput>
+              </IonItem>
+            </IonCol>
+          </IonRow>
+          <br></br>
+          <IonRow className="ion-justify-content-center">
+            <IonCol sizeXs="12" sizeMd="6">
+              <IonItem>
+                <IonLabel position="floating">Subject</IonLabel>
+                <IonInput
+                  onIonChange={(e) => {
+                    this.setState({ subject: e.detail.value! });
+                  }}
+                ></IonInput>
+              </IonItem>
+            </IonCol>
+          </IonRow>
+          <IonRow className="ion-justify-content-center">
+            <IonCol sizeXs="12" sizeMd="6">
+              <IonItem>
+                <IonLabel position="floating">Phone</IonLabel>
+                <IonInput
+                  onIonChange={(e) => {
+                    this.setState({ phone: e.detail.value! });
+                  }}
+                ></IonInput>
+              </IonItem>
+            </IonCol>
+          </IonRow>
+          <br></br>
+          <IonRow className="ion-justify-content-center">
+            <IonCol sizeXs="12" sizeMd="6">
+              <IonItem>
+                <IonTextarea
+                  placeholder="Enter classroom Description"
+                  onIonChange={(e) => {
+                    this.setState({ description: e.detail.value! });
+                  }}
+                ></IonTextarea>
+              </IonItem>
+            </IonCol>
+          </IonRow>
+          <br></br>
+          <IonRow className="ion-justify-content-center">
+            <IonCol sizeXs="12" sizeMd="3">
+              <IonButton size="large" expand="full" onClick={this.create}>
+                Create
+              </IonButton>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
+
+        <br></br>
+        <p>
+          Want to join an existing classroom?{" "}
+          <Link to="/page/join" className="noUnderline">
+            Join Here
+          </Link>
+        </p>
+      </IonContent>
+    );
+  }
+}
+
+const mapStateToProps = (state: any) => {
+  return {
+    userID: state.userReducer.user ? state.userReducer.user.userID : "",
+  };
 };
 
-interface State{
-    classroomName:string,
-    subject:string,
-    description:string
-}
-
-class CreateClassroom extends Component<Props,State>{
-    constructor(props:Props){
-        super(props);
-        this.state={
-            classroomName:'',
-            subject:'',
-            description:''
-        }
-    }
-
-    create=()=>{
-        const classroom={
-            classroomName:this.state.classroomName,
-            subject:this.state.subject,
-            description:this.state.description,
-            email:this.props.email
-        }
-
-        // api request
-        axios.post("/classrooms/create",classroom)
-        .then(response=>{
-            this.props.history.push("/page/classrooms");
-        })
-        .catch(err=>{
-            console.log(err);
-            window.alert("error in creating classroom");
-        })
-    }
-
-    render(){
-        // console.log("classroomName is "+this.state.classroomName);
-        // console.log("subject is "+this.state.subject);
-        // console.log("description is "+this.state.description);
-
-        return(
-            <div className="ion-text-center">
-                <h1>Create Classroom</h1>
-                <br></br>
-                <IonGrid>
-                    <IonRow className="ion-justify-content-center">
-                        <IonCol sizeXs="12" sizeMd="6">
-                            <IonItem>
-                                <IonLabel position="floating">Classroom Name</IonLabel>
-                                <IonInput onIonChange={(e) => { this.setState({classroomName:e.detail.value!}) }}></IonInput>
-                            </IonItem>
-                        </IonCol>
-                    </IonRow>
-                    <br></br>
-                    <IonRow className="ion-justify-content-center">
-                        <IonCol sizeXs="12" sizeMd="6">
-                            <IonItem>
-                                <IonLabel position="floating">Subject</IonLabel>
-                                <IonInput onIonChange={(e) => { this.setState({subject:e.detail.value!}) }}></IonInput>
-                            </IonItem>
-                        </IonCol>
-                    </IonRow>
-                    <br></br>
-                    <IonRow className="ion-justify-content-center">
-                        <IonCol sizeXs="12" sizeMd="6">
-                            <IonItem>
-                                <IonTextarea placeholder="Enter classroom Description" onIonChange={(e) => { this.setState({description:e.detail.value!}) }}></IonTextarea>
-                            </IonItem>
-                        </IonCol>
-                    </IonRow>
-                    <br></br>
-                    <IonRow className="ion-justify-content-center">
-                        <IonCol sizeXs="12" sizeMd="3">
-                            <IonButton size="large" expand="full" onClick={this.create}>
-                                Create
-                            </IonButton>
-                        </IonCol>
-                    </IonRow>
-                </IonGrid>
-                
-                <br></br>
-                <p>Want to join an existing classroom? <Link to="/page/join" className="noUnderline">Join Here</Link></p>
-            </div>
-        );
-    }
-}
-
-const mapStateToProps=(state:any)=>{
-    return{
-        email:(state.userReducer.user?state.userReducer.user.email:'')
-    }
-}
-
-export default connect(mapStateToProps,null)(CreateClassroom);
+export default connect(mapStateToProps, null)(CreateClassroom);
