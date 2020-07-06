b6628b8a8af458d3ed9d706f8e9db2ee7064d93c
Aadhar <tyagi.aadhar@gmail.com>
2020-07-06 17:36:22 +0530

    fix announcement dark mode and scroll

--- client/src/pages/Classrooms/Announcements/Announcements.tsx
+++ client/src/pages/Classrooms/Announcements/Announcements.tsx
@@ -1,50 +1,66 @@
-import React, { useState, useEffect } from 'react';
-import AnnouncementItem from './AnnouncementItem';
-import axios from 'axios';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import AnnouncementItem from "./AnnouncementItem";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import { IonPage, IonContent } from "@ionic/react";
 
-const Announcements:React.FC<{ match:any }> = ({ match }) => {
-    const [announcementList, setAnnouncementList] = useState([]);
-    
-    const classroomID=match.params.classroomID;
-    const loadAllAnnouncements = () =>{
-        axios.get(`/classrooms/getAllAnnouncements/${classroomID}`)
-        .then((response:any)=>{
-            setAnnouncementList(response.data.announcements);
-        });
-    }
+const Announcements: React.FC<{ match: any }> = ({ match }) => {
+  const [announcementList, setAnnouncementList] = useState([]);
 
-    useEffect(()=>{
-        loadAllAnnouncements();
-    },[]);
+  const classroomID = match.params.classroomID;
+  const loadAllAnnouncements = () => {
+    axios
+      .get(`/classrooms/getAllAnnouncements/${classroomID}`)
+      .then((response: any) => {
+        setAnnouncementList(response.data.announcements);
+      });
+  };
 
-    let reversedAnnouncementList = announcementList;
-    if(announcementList && announcementList.slice){
-        // announcementList.sort((a:any,b:any)=>{ 
-        //     if(new Date(b.postedDate) >= new Date(a.postedDate)){
-        //         return 1;
-        //     }
-        //     else{
-        //         return -1;
-        //     }
-        // });
-        reversedAnnouncementList = announcementList.slice().reverse();
-    }
-    console.log(reversedAnnouncementList);
-    return(
+  useEffect(() => {
+    loadAllAnnouncements();
+  }, []);
+
+  let reversedAnnouncementList = announcementList;
+  if (announcementList && announcementList.slice) {
+    // announcementList.sort((a:any,b:any)=>{
+    //     if(new Date(b.postedDate) >= new Date(a.postedDate)){
+    //         return 1;
+    //     }
+    //     else{
+    //         return -1;
+    //     }
+    // });
+    reversedAnnouncementList = announcementList.slice().reverse();
+  }
+  console.log(reversedAnnouncementList);
+  return (
+    <IonPage>
+      <IonContent>
         <div className="ion-text-center">
-            <h1>Announcements</h1>
-            <Link to="/page/classrooms" className="noUnderline">Go back to classrooms</Link>
-            {
-                (reversedAnnouncementList && reversedAnnouncementList.map && reversedAnnouncementList.length)?
-                reversedAnnouncementList.map((a:any)=>{
-                    return(
-                        <AnnouncementItem announcement={a} key={a._id} classroomID={classroomID} loadAllAnnouncements={loadAllAnnouncements}></AnnouncementItem>
-                    );
-                }):<p>No Announcements</p>
-            }
+          <h1>Announcements</h1>
+          <Link to="/page/classrooms" className="noUnderline">
+            Go back to classrooms
+          </Link>
+          {reversedAnnouncementList &&
+          reversedAnnouncementList.map &&
+          reversedAnnouncementList.length ? (
+            reversedAnnouncementList.map((a: any) => {
+              return (
+                <AnnouncementItem
+                  announcement={a}
+                  key={a._id}
+                  classroomID={classroomID}
+                  loadAllAnnouncements={loadAllAnnouncements}
+                ></AnnouncementItem>
+              );
+            })
+          ) : (
+            <p>No Announcements</p>
+          )}
         </div>
-    );
-}
+      </IonContent>
+    </IonPage>
+  );
+};
 
 export default Announcements;
